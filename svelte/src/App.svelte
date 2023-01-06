d3a3b8d95894fa74344ff9433bd6bec6cd03ab26
lingeshwaran05 <lingeshkrish08@gmail.com>
2023-01-06 22:51:12 +0530

    created home page

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,33 +1,91 @@
 <script>
 import bg from './assets/7741849_3747371.jpg'
 import logo from './assets/ll.png'
+  import Card from './card.svelte';
+import card from './card.svelte'
+let url="Enter URL to check ";
 </script>
 
 <main>
  <nav> <ul>  
-    <li><a href="about.asp">About Us</a></li>
-    <li><a href="contact.asp">Contact Us</a></li>
-    <li><a href="news.asp">Docs</a></li>
-    <li id="home"><a href="default.asp"><img src={logo} width="20" alt=""> Link Checker</a></li>
+    <li><a href="#about">About Us</a></li>
+    <li><a href="#contact">Contact Us</a></li>
+    <li><a href="#docs">Docs</a></li>
+    <li id="home"><a href="#MAIN"><img src={logo} width="20" alt=""> Link Checker</a></li>
   </ul>
 </nav>
-<p id="p">404 IS <span id="bad"><u>BAD</u> </span>FOR BUSINESS.</p>
+<section id="MAIN" >
+<p id="p">404 IS <span id="bad"><u>BAD</u> </span>FOR BUSINESS 😓 .</p>
 <div id="bg">
   <img src={bg} alt="" class="center">
 </div>
 <div class="url">
-  <input type="text" class="center" value="Enter URL to check">
+  <input type="text" class="center" bind:value={url}>
 </div>
+<br>
+<p class="center" id="display">Website URL : 👉   {url}</p>
+<br>
+<br>
 <button>Submit</button>
+
+</section>
+<br>
+<br>
+<br>
+<section id="about">
+  <Card/>
+</section>
+<br>
+<br>
+<br>
+<section id="contact">
+<Card/>
+</section>
+<footer>
+  <p class="cp-text">
+    © Copyright 2023 linkchecker. All rights reserved.
+  </p>
+</footer>
 </main>
 
 <style>
 #bad{
   color: red;
 }
+#display{
+text-shadow: #111;
+font-size: large;
+font-weight: bolder;
+}
+footer{
+  background-color: #111;
+  color: aliceblue;
+   text-align: center;
+  padding: 2px 3px;
+  display: block;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+button{  
+display: block;
+margin: 0 auto;
+color: rgb(255, 255, 255);
+border-color:blue ;
+background-color: #111;
+font-size: 20px;
+transition: all 2s ease-out;
+}  
+button:hover{
+  position: relative;
+  top: -3px;
+  box-shadow: -6px 14px 12px 2px rgba(90, 90, 90, .12);
+}
 input{
   color: gray;
   font-size: 20px;
+  background-color: rgb(251, 245, 236);
+  border-radius: 5px;
 }
 .center {
   display: block;
